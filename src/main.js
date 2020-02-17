ec70b9c9cec843c5b8e095a57ad02cc0393cd078
CZR <1767939301@qq.com>
2020-02-17 21:03:15 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+//导入全局样式表
+import './assets/css/global.css'
+//将axios提交数据时的默认json格式改为表单格式提交
+import qs from 'qs'
+import axios from 'axios'
+
+
+axios.defaults.baseURL = 'http://api.auth.com'
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+Vue.prototype.$qs = qs
+Vue.prototype.$http = axios
+
 
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
+
+//导入弹框提示组件message,需要挂载到Vue原型
+Vue.prototype.$message = Message
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
